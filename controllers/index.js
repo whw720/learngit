fbd5c01ddf888403806806b1311f7fe2f7b22db6
whw720 <whw720@163.com>
2016-03-30 13:30:01 +0800

    add code

--- controllers/index.js
+++ controllers/index.js
@@ -1,4 +1,4 @@
-var jsSHA=require('jssha');
+var crypto = require('crypto');
 exports.index = function(req, res) {
     var baseUrl=req.protocol + '://' + req.get('Host');
 	res.render('index', {
@@ -9,24 +9,27 @@ exports.index = function(req, res) {
 };
 exports.weixin = function(req, res) {
 	var signature=req.query.signature; //微信加密签名
-	var token=whw720school;
+	var token='whw720school';
 	var timestamp=req.query.timestamp; //时间
 	var nonce=req.query.nonce;//随机数
 	var echostr=req.query.echostr; //随机字符串
-	console.log('signature:'+signature+' time:'+timeStamp+' nonce:'+nonce+' echostr:'+echostr);
+	console.log('signature:'+signature+' time:'+timestamp+' nonce:'+nonce+' echostr:'+echostr);
 	var oriArray = new Array();
 	oriArray[0] = nonce;
 	oriArray[1] = timestamp;
 	oriArray[2] = token;
 	oriArray.sort();
 	var original = oriArray.join('');
-	var shaObj = new jsSHA(original, 'TEXT');
-	var scyptoString=shaObj.getHash('SHA-1', 'HEX'); 
-	if(signature == scyptoString){
+	//2. 将三个参数字符串拼接成一个字符串进行sha1加密
+	var sha1Code = crypto.createHash("sha1");
+	var code = sha1Code.update(original,'utf-8').digest("hex");
+//	var shaObj = new jsSHA(original, 'TEXT');
+//	var scyptoString=shaObj.getHash('SHA-1', 'HEX'); 
+	if(signature == code){
  	//验证成功
 	 res.end(echostr);  
 	} else {
 	 //  //验证失败
-	 	 res.sendResult('fail');
+	 	 res.end('fail');
 	    }
 }
